3c15e3c1250b91be6f3676224c6b620331000dc9
araviimakesitcool <araviimakesitcool@gmail.com>
2025-03-11 17:07:04 +0530

    Initial commit

---
+++ src/main/java/com/example/JenkinsTester/JenkinsTesterApplication.java
@@ -0,0 +1,13 @@
+package com.example.JenkinsTester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JenkinsTester/JenkinsTesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JenkinsTester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
